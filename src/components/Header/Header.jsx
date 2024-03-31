162cbcf3bb6d39415cd13c43f8619b487e3e8d92
senice222 <artemprimorksij7@gmail.com>
2024-03-31 12:26:04 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import style from './Header.module.scss'
 import filter from '../../assets/filter.png'
- 
+
 const Header = () => {
 
     return (
         <div className={style.header}>
             <div className={style.filtersDiv}>
-                <img src={filter} alt="/" /> 
+                <div className={style.container}>
+                    <img src={filter} alt="/" />
+                </div>
             </div>
 
             <div className={style.searchDiv}>
-                <input className={style.serchInput} placeholder='Поиск...'/>
+                <input className={style.serchInput} placeholder='Поиск...' />
             </div>
         </div>
     );
