56081df0bd2ad75c27f3f363bda4d80f40f9fb43
senice222 <artemprimorksij7@gmail.com>
2024-04-01 21:35:15 +0300

    updt

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -37,7 +37,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId
         }
-        fetch('http://localhost:8000', {
+        fetch('http://89.208.103.148:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
