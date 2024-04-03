c34019d0b2e798e213e3ce3a32f730e57ada1675
senice222 <artemprimorksij7@gmail.com>
2024-04-03 03:55:32 +0300

    updt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,24 @@ import { products } from '../../utils/products';
 
 const Header = () => {
     const [value, setValue] = useState("")
+    const [productsList, setProductsList] = useState()
     const navigate = useNavigate()
     const debouncedValue = useDebounce(value, 300)
     const [isOpen, setIsOpen] = useState(true);
 
+    useEffect(() => {
+        const getProducts = () => {
+            const data = products.filter(item => item.title.toLowerCase().includes(debouncedValue.toLowerCase()))
+            setProductsList(data)
+        }
+        getProducts()
+    }, [debouncedValue])
+
     const itemClickHandler = (e) => {
         const clickedProduct = e.target.textContent
         setValue(clickedProduct)
-        const choseUser = products.filter(item => item.title === clickedProduct)
+        const choseUser = productsList.filter(item => item.title === clickedProduct)
+        console.log(choseUser)
         const productUrl = `/profile`
         if (clickedProduct) {
             navigate(productUrl, { replace: true })
@@ -41,7 +51,7 @@ const Header = () => {
                 <div className={style.container}>
                     <ul className={style.ulAutoComplete}>
                         {
-                            (products && isOpen) && products.map((item, i) => (
+                            (productsList && isOpen) && productsList.map((item, i) => (
                                 <div key={i}>
                                     <li
                                         onClick={itemClickHandler}
